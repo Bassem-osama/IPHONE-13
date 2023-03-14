510068270c106a8db5a08e3dce9a7f1e0ba7d141
bassem <bassemosama75@gmail.com>
2023-03-14 22:55:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let img = document.getElementById('image');
+let container = document.querySelector('.container');
+let text = document.querySelector('.text');
+let btn = document.getElementById('btn');
+
+function phones(phone) {
+    img.src = phone;
+}
+
+function colors(c) {
+    container.style.background = c;
+    
+}
